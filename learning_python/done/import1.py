e14a457ea5c6262476456034648cf892c9052b45
YukiYoshizawa <eatermgsorigin4@gmail.com>
2025-04-15 17:37:50 +0900

    L17

--- learning_python/done/import1.py
+++ learning_python/done/import1.py
@@ -1,4 +1,4 @@
-import tax       #手順１で作ったモジュール(ファイル名の.pyより前の部分を指定する)
+import done.tax as tax       #手順１で作ったモジュール(ファイル名の.pyより前の部分を指定する)
 
 print(tax.postTaxPrice(120), "円")      #関数を呼び出すときは「モジュール名.関数名」
 print(tax.postTaxPrice(128), "円")
